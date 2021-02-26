e57eba161bf6001126c3408beeaaaebf7639b3e0
Michael Boone <45855299+MDBoone@users.noreply.github.com>
2021-02-26 12:02:11 -0700

    Create AiPad.java

---
+++ AiPad.java
@@ -0,0 +1,45 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class AiPad implements Pad{
+	
+	double y, yVel;
+	boolean upAcc, downAcc;
+	int player, x;
+	final double GRAVITY = 0.94;
+	Ball b1;
+	
+	public AiPad(int player, Ball b) {
+		b1 = b;
+		upAcc = false;
+		downAcc = false;
+		y = 210;
+		yVel = 0;
+		if(player == 1) {
+			x = 20;
+		}
+		else {
+			x = 660;
+		}
+	}
+	public void draw(Graphics g) {
+		g.setColor(Color.white);
+		g.fillRect(x, (int)y, 20, 80);
+	}
+
+	public void move() {
+		y = b1.getY() - 40;
+		if(y < 0) {
+			y = 0;
+		}
+		
+		if(y > 520) {
+			y = 520;
+		}
+		
+	}
+	
+	public int getY() {
+		return (int)y;
+	}
+}
